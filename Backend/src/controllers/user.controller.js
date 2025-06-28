247bd502e1e37e1c7e0fd3341704cf5a4139bf52
Divyesh Gangera <divyeshgangera22@gmail.com>
2025-06-28 19:39:08 +0530

    feat: implement user registration route and controller

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+ import { asyncHandler } from "../utils/asyncHandler.js";
+
+ const registerUser = asyncHandler( (req,res ) => {
+    res.status(200).json({
+        message: "Ok You are Register"
+    })
+ } ); 
+
+
+ export { 
+    registerUser,
+ }
